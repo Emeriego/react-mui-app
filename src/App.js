2e3e7c4b65f3686a779b49b53645341ff0a9a4ce
emeriego@gmail.com <86321701+Emeriego@users.noreply.github.com>
2023-07-28 00:54:02 +0100

    random updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import { Button } from "@mui/material";
-import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
+import { Button, Typography } from "@mui/material";
+import {Alarm, Add, AlarmAdd} from '@mui/icons-material';
 
 
 function App() {
+//git remote add <url copied from your github is pasted here>
+//this command will link your git with the specified repository.
   return (
     <div className="App">
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color= "secondary" startIcon = {<AccessAlarmsIcon/>}>Contained</Button>
-      <Button variant="outlined" endIcon = {<AccessAlarmsIcon/>}>Outlined</Button>
+      <Button variant="text" endIcon={<Add/>}>Text</Button>
+      <Button variant="contained" color= "secondary" startIcon = {<AlarmAdd/>}>Contained</Button>
+      <Button variant="outlined" endIcon = {<Alarm/>}>Outlined</Button>
+
+      <Typography variant = "h1">
+        This is how h2 is tagged. The variant gives you the style.
+
+      </Typography>
     </div>
   );
 }
